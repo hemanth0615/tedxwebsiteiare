b061ea823ab5631faf4efd800f73109f2485aa63
hemanth0615 <mallahemanthnaidu2001@gmail.com>
2022-12-24 12:53:33 +0530

    changes

--- src/pages/HomePage/Home.jsx
+++ src/pages/HomePage/Home.jsx
@@ -113,8 +113,8 @@ const allFunction = () => {
               In the spirit of ideas worth spreading, TEDx is a program of local, self-organized events that bring people together to share a TED-like experience. At a TEDx event, TED Talks video and live speakers combine to spark deep discussion and connection. These local, self-organized events are branded TEDx, where x = independently organized TED event. The TED Conference provides general guidance for the TEDx program, but individual TEDx events are self-organized. (Subject to certain rules and regulations.)
             </p>
             <div className="home_about_end_desc">
-          <p>“IDEAS WORTH SPREADING”</p>
-        </div>
+              <p>“x=independently organised event”</p>
+            </div>
           </div>
           <div
             style={{ display: contentactive ? "" : "none" }}
