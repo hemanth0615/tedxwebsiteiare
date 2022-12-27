5f1f37a1f7ac2a17960f66eb043e5ad06fe3ca0b
Nagateja <teja91.valiveti@gmail.com>
2022-12-27 20:44:28 +0530

    src code changed completly

---
+++ src/Speakers.js
@@ -0,0 +1,86 @@
+import DummyPotrait from "./Assets/DummyPotrait.jpg";
+
+const speakers = [
+    {
+        id : 1,
+        name : 'John Doe',
+        role : 'VISUAL ARTIST, COMPOSER & DIRECTOR',
+        linkedin : 'linkedin.com/profile',
+        twitter : 'twitter.com/profile',
+        image : DummyPotrait
+    },
+    {
+        id : 2,
+        name : 'John Doe',
+        role : 'VISUAL ARTIST, COMPOSER & DIRECTOR',
+        linkedin : 'linkedin.com/profile',
+        twitter : 'twitter.com/profile',
+        image : DummyPotrait
+    },
+    {
+        id : 3,
+        name : 'John Doe',
+        role : 'VISUAL ARTIST, COMPOSER & DIRECTOR',
+        linkedin : 'linkedin.com/profile',
+        twitter : 'twitter.com/profile',
+        image : DummyPotrait
+    },
+    {
+        id : 4,
+        name : 'John Doe',
+        role : 'VISUAL ARTIST, COMPOSER & DIRECTOR',
+        linkedin : 'linkedin.com/profile',
+        twitter : 'twitter.com/profile',
+        image : DummyPotrait
+    },
+    {
+        id : 5,
+        name : 'John Doe',
+        role : 'VISUAL ARTIST, COMPOSER & DIRECTOR',
+        linkedin : 'linkedin.com/profile',
+        twitter : 'twitter.com/profile',
+        image : DummyPotrait
+    },
+    {
+        id : 6,
+        name : 'John Doe',
+        role : 'VISUAL ARTIST, COMPOSER & DIRECTOR',
+        linkedin : 'linkedin.com/profile',
+        twitter : 'twitter.com/profile',
+        image : DummyPotrait
+    },
+    {
+        id : 7,
+        name : 'John Doe',
+        role : 'VISUAL ARTIST, COMPOSER & DIRECTOR',
+        linkedin : 'linkedin.com/profile',
+        twitter : 'twitter.com/profile',
+        image : DummyPotrait
+    },
+    {
+        id : 8,
+        name : 'John Doe',
+        role : 'VISUAL ARTIST, COMPOSER & DIRECTOR',
+        linkedin : 'linkedin.com/profile',
+        twitter : 'twitter.com/profile',
+        image : DummyPotrait
+    },
+    {
+        id : 9,
+        name : 'John Doe',
+        role : 'VISUAL ARTIST, COMPOSER & DIRECTOR',
+        linkedin : 'linkedin.com/profile',
+        twitter : 'twitter.com/profile',
+        image : DummyPotrait
+    },
+    {
+        id : 10,
+        name : 'John Doe',
+        role : 'VISUAL ARTIST, COMPOSER & DIRECTOR',
+        linkedin : 'linkedin.com/profile',
+        twitter : 'twitter.com/profile',
+        image : DummyPotrait
+    },
+]
+
+export default speakers;
